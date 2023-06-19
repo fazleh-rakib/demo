d43912e560c8446d44813b249ebeb75266353629
Faz Rak <77926707+fazleh-rakib@users.noreply.github.com>
2023-06-20 00:33:20 +0600

    2nd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+
+          secondary: "#19D3AE",
+
+          accent: "##3A4256",
+
+          neutral: "#021431",
+
+          "base-100": "#ffffff",
+
+          info: "#93e6fb",
+
+          success: "#80ced1",
+
+          warning: "#efd8bd",
+
+          error: "#e58b8b",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
